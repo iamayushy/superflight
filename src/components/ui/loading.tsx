d7555fef675f747afa2d83c14f987e0a5c5aadbc
iamayushy <ayushhuns@gmail.com>
2025-07-24 19:31:55 +0530

    loader added

--- src/components/ui/loading.tsx
+++ src/components/ui/loading.tsx
@@ -1,6 +1,10 @@
-function Loader () {
-    return <div>
-        loading....
+
+function loader() {
+  return (
+    <div className="fixed inset-0 z-50 bg-white flex items-center justify-center">
+      <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
     </div>
+  );
 }
-export default Loader;
+
+export default loader;
